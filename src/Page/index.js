2ba72b65f1b766309a33022d3d02c9f273a68fdc
Tom Kiernan <tkiernan120@gmail.com>
2018-11-15 13:30:06 +0000

    WIP

--- src/Page/index.js
+++ src/Page/index.js
@@ -2,5 +2,6 @@ import LandingPage from './LandingPage'
 import SignUpPage from './SignUpPage'
 import SignIn from './SignIn'
 import Home from './Home'
+import Account from './Account'
 
-export { LandingPage, SignUpPage, SignIn, Home }
+export { LandingPage, SignUpPage, SignIn, Home, Account }
