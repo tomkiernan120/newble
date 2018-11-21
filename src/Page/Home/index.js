660743be05658b87644272f59d7ff79f869fb032
Tom Kiernan <tkiernan120@gmail.com>
2018-11-21 13:55:26 +0000

    WIP

--- src/Page/Home/index.js
+++ src/Page/Home/index.js
@@ -1,20 +1,32 @@
 import React from "react";
 import { CopyToClipboard } from "react-copy-to-clipboard";
+
+// import {CodeMirrorReact as CodeMirror } from '../../Components';
+
 import CodeMirror from 'react-codemirror';
+import "../../../node_modules/codemirror/lib/codemirror.css";
 
 import WithAuthorization from "../../Components/WithAuthorization";
 
 
 import { db, firebase } from "../../firebase";
 
-import "../../../node_modules/codemirror/lib/codemirror.css"
-console.log( WithAuthorization )
+require('codemirror/mode/javascript/javascript');
+require('codemirror/mode/xml/xml');
+require('codemirror/mode/css/css');
+require('codemirror/mode/go/go');
+require('codemirror/mode/htmlmixed/htmlmixed');
+require('codemirror/mode/sass/sass');
+
+// import "../../../node_modules/codemirror/lib/codemirror.css"
 
 const INITIAL_STATE = {
   toggleShow: false,
   title: "",
   snippet: "",
   errors: null,
+  type: '',
+  types: ['javascript','xml','css','go','html', 'php','htmlmixed','sass'],
   snippets: null
 };
 
@@ -28,6 +40,7 @@ class HomePage extends React.Component {
     this.handleClose = this.handleClose.bind(this);
     this.addSnippet = this.addSnippet.bind(this);
     this.removeSnippet = this.removeSnippet.bind(this);
+    this.snippetChange = this.snippetChange.bind(this);
   }
 
   componentDidMount() {
@@ -62,6 +75,11 @@ class HomePage extends React.Component {
 
   toggleShow() {
     this.setState({ toggleShow: !this.state.toggleShow });
+    this.setState({ type: '' });
+  }
+
+  snippetChange(value){
+    this.setState({ snippet: value })
   }
 
   handleChange(e) {
@@ -115,7 +133,10 @@ class HomePage extends React.Component {
             close={this.handleClose}
             toggleShow={this.state.toggleShow}
             handleChange={this.handleChange}
+            snippetChange={this.snippetChange}
+            currentType={this.state.type}
             title={this.state.title}
+            types={this.state.types}
             snippet={this.state.snippet}
           />
           <div className="snippet-container">
@@ -128,8 +149,8 @@ class HomePage extends React.Component {
                         <strong>{e.title}</strong>
                       </p>
                       <p>{e.time}</p>
-                    </div>
-                    <CodeMirror value={e.snippet} options={options} />
+                    </div>  
+                    <CodeMirror options={options} />
                     <CopyToClipboard text={e.snippet}>
                       <button>Copy</button>
                     </CopyToClipboard>
@@ -147,12 +168,22 @@ class HomePage extends React.Component {
 }
 
 class Modal extends React.Component {
+
   render() {
     if (this.props.toggleShow) {
       document.body.style.overflow = "hidden";
-    } else {
+    } 
+    else {
       document.body.style.overflow = "";
     }
+
+    const options = { smartIndent: true, lineNumbers:true }
+
+    if( this.props.type ){
+      options.mode = this.props.type;
+    }
+
+    console.log( options );
 
     return (
       <div data-toggle={this.props.toggleShow}>
@@ -168,12 +199,14 @@ class Modal extends React.Component {
               name="title"
               value={this.props.title}
             />
+            <label>Type</label>
+            <select name="type" onChange={this.props.handleChange} value={this.props.currentType}>
+              {this.props.types.map((e,i) => {
+                return <option key={i} value={e}>{e}</option>
+              })}
+            </select>
             <label>Snippet</label>
-            <textarea
-              onChange={this.props.handleChange}
-              name="snippet"
-              value={this.props.snippet}
-            />
+            <CodeMirror name="popup" options={options} style={{float:'left'}} onChange={this.props.snippetChange} name="snippet" value={this.props.snippet} id="popup" />
             <button onClick={this.props.addSnippet}>Add Snippet</button>
           </div>
         </div>
