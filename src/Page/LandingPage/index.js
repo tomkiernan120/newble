3208a7fcba41f93e6f6449c03707c8f455f9bbd5
Tom Kiernan <tkiernan120@gmail.com>
2018-11-26 17:23:28 +0000

    WIP

--- src/Page/LandingPage/index.js
+++ src/Page/LandingPage/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import Particles from "react-particles-js";
 
 export default class LandingPage extends React.Component {
@@ -34,7 +34,18 @@ export default class LandingPage extends React.Component {
           >
             Super Simple code snippet library
           </h1>
-          <p style={{textAlign:"center"}}><Link to="/signin">Login</Link> to manage your snippets, or <Link to="/signup">Sign up</Link></p>
+          <p
+            style={{
+              textAlign: "center",
+              position: "absolute",
+              top: "57%",
+              left: "50%",
+              zIndex: 99999,
+              transform: "translate( -50%, -50% )"
+            }}
+          >
+            <Link to="/signin">Login</Link> to manage your snippets, or <Link to="/signup">Sign up</Link>
+          </p>
         </div>
       </section>
     );
